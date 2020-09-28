7629db5f32927410752ee25b9ba03721c1d31cb3
Arudovwen <successahon@gmail.com>
2020-09-28 23:31:13 +0100

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
-const con = mysql.createConnection({
-    host:'us-cdbr-east-02.cleardb.com',
-    user:'b9c3a0870852f4',
-    password:'06f354c2',
-    database:'heroku_32656126bc1fe7a'
-})
+// const con = mysql.createConnection({
+//     host:'us-cdbr-east-02.cleardb.com',
+//     user:'b9c3a0870852f4',
+//     password:'06f354c2',
+//     database:'heroku_32656126bc1fe7a'
+// })
 
 // const con = mysql.createConnection({
 //   host: "127.0.0.1",
@@ -13,12 +13,25 @@ const con = mysql.createConnection({
 //   database: "polybuckz",
 // });
 
-con.connect((err) => {
-  if (err) {
-    console.log("Error connecting to Db");
-    return;
-  }
-  console.log("Connection established");
+
+// con.connect((err) => {
+//   if (err) {
+//     console.log("Error connecting to Db");
+//     return;
+//   }
+//   console.log("Connection established");
+// });
+
+
+const con = mysql.createPool({
+  host:'us-cdbr-east-02.cleardb.com',
+    user:'b9c3a0870852f4',
+    password:'06f354c2',
+    database:'heroku_32656126bc1fe7a'
 });
+
+// ... later
+con.query('select 1 + 1', (err, rows) => { /* */ });
+
 
 module.exports = con;
