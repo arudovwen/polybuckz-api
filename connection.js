4db0b0b13cd421407f0f6ec4de164942d583c46e
Arudovwen <successahon@gmail.com>
2020-09-28 19:14:36 +0100

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const con = mysql.createConnection({
-    host:'127.0.0.1',
-    user:'root',
-    password:'',
-    database:'polybuckz'
+    host:'us-cdbr-east-02.cleardb.com',
+    user:'b6bbea201c07dd',
+    password:'09c00c2c',
+    database:'heroku_fa446684ae8a265'
 })
 
 con.connect((err) => {
